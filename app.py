39545b5876bf278c8e47ddb20d510a62858efa86
felipetunesvader <felipe.antunes48@hotmail.com>
2023-10-07 03:48:41 -0300

    conexão com data base(postgres), inserção de dados na tabela e output do dataframe pandas

--- app.py
+++ app.py
@@ -5,13 +5,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import psycopg2
+import pandas as pd  # Importação do Pandas
 
 logging.basicConfig(level=logging.INFO)
+
+
+def connect_to_db():
+    connection = psycopg2.connect(
+        host="localhost",
+        database="movies_db",
+        user="postgres",
+        password="modric19"
+    )
+    return connection
+
 
 def initialize_webdriver():
     driver = webdriver.Chrome()
     return driver
+
 
 def extract_top_movie_links(num=1):
     URL = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
@@ -23,6 +36,7 @@ def extract_top_movie_links(num=1):
 
     links = driver.find_elements(By.CSS_SELECTOR, "li.ipc-metadata-list-summary-item a.ipc-title-link-wrapper")[:num]
     return [link.get_attribute("href") for link in links]
+
 
 def extract_movie_details(url, driver):
     driver.get(url)
@@ -39,12 +53,15 @@ def extract_movie_details(url, driver):
         title = "N/A"
 
     try:
-        year = driver.find_element(By.CSS_SELECTOR, "a.ipc-link.ipc-link--baseAlt.ipc-link--inherit-color[href*='releaseinfo']").text
+        year = driver.find_element(By.CSS_SELECTOR,
+                                   "a.ipc-link.ipc-link--baseAlt.ipc-link--inherit-color[href*='releaseinfo']").text
     except:
         year = "N/A"
 
     try:
-        rating_value = driver.find_element(By.CSS_SELECTOR, "span.ipc-rating-star.ipc-rating-star--base.ipc-rating-star--imdb").text.split()[0]
+        rating_value = driver.find_element(By.CSS_SELECTOR,
+                                           "span.ipc-rating-star.ipc-rating-star--base.ipc-rating-star--imdb").text.split()[
+            0]
     except:
         rating_value = "N/A"
 
@@ -74,8 +91,25 @@ def save_to_csv(data, filename="output.csv"):
         dict_writer.writeheader()
         dict_writer.writerows(data)
 
-def take_screenshot(driver, filename="screenshot.png"):
-    driver.save_screenshot(filename)
+def insert_into_table(connection, movie_data):
+    cursor = connection.cursor()
+
+    for movie in movie_data:
+        cursor.execute('''
+            INSERT INTO movies (title, year, rating, genres, director, writers, stars)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+        ''', (movie["title"], movie["year"], movie["rating"], movie["genres"], movie["director"], movie["writers"],
+              movie["stars"]))
+
+    connection.commit()
+    cursor.close()
+
+
+def visualize_data_with_pandas(connection):
+    # Usar Pandas para ler os dados do banco de dados
+    df = pd.read_sql('SELECT * FROM movies', connection)
+    print(df.head(10))  # Visualizar os primeiros 10 registros
+
 
 if __name__ == "__main__":
     logging.info("Iniciando WebDriver...")
@@ -94,10 +128,16 @@ if __name__ == "__main__":
         all_movie_details.append(movie_details)
         logging.info(movie_details)
 
-        screenshot_filename = movie_details["title"].replace(" ", "_") + ".png"
-        take_screenshot(driver, screenshot_filename)
-
     save_to_json(all_movie_details, "filmes.json")
     save_to_csv(all_movie_details, "filmes.csv")
 
     driver.close()
+
+    connection = connect_to_db()
+    insert_into_table(connection, all_movie_details)
+
+    # Adicionando a chamada da função de visualização após inserção no banco de dados
+    visualize_data_with_pandas(connection)
+
+    connection.close()
+    logging.info("Dados inseridos no banco de dados com sucesso!")
---
+++ setup_db.py
@@ -0,0 +1,38 @@
+import psycopg2
+
+
+def connect_to_db():
+    connection = psycopg2.connect(
+        host="localhost",
+        database="movies_db",
+        user="postgres",
+        password="modric19"
+    )
+    return connection
+
+
+def create_table(connection):
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS movies (
+            id SERIAL PRIMARY KEY,
+            title TEXT,
+            year TEXT,
+            rating TEXT,
+            genres TEXT[],
+            director TEXT[],
+            writers TEXT[],
+            stars TEXT[]
+        )
+    ''')
+
+    connection.commit()
+    cursor.close()
+
+
+if __name__ == "__main__":
+    connection = connect_to_db()
+    create_table(connection)
+    connection.close()
+    print("Tabela criada com sucesso!!!")
